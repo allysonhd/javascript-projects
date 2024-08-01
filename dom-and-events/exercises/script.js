259e9cc5eff9d8e3d86928fff525c151b83bf91c
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-08-01 08:22:10 -0500

    homework start

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,12 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', function (event) {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseover", function (event) {
+        button.style.backgroundColor = "red" //am I also supposed to be coding in the style.css?  Otherwise, I don't understand how to set this color.
+     });
 }
-
-window.addEventListener("load", init);
+ //when the button is clicked, the <p id="statusReport">The shuttle is on the ground</p> changes to <p id="statusReport">Houston, we have liftoff!</p>
+ //when the curson goes over id="abortMission" the button background turns red
