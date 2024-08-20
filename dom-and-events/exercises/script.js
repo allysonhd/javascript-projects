946b5ae0fa0169e8b7324a787808f238aea944a3
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-08-20 16:33:40 -0500

    fixed event listener for mission abort

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,16 +4,16 @@ function init() {
   const paragraph = document.getElementById("statusReport");
 
   // Put your code for the exercises here.
-  button.addEventListener("click", function (event) {
+  button.addEventListener("click", function () {
     paragraph.innerHTML = "Houston! We have liftoff!";
   });
-  missionAbort.addEventListener("mouseover", function (event) {
-    button.target.style.backgroundColor = "red";
+  missionAbort.addEventListener("mouseover", function () {
+    missionAbort.style.backgroundColor = "red";
   });
-  missionAbort.addEventListener("mouseout", function (event) {
-    button.target.style.backgroundColor = "";
+  missionAbort.addEventListener("mouseout", function () {
+    missionAbort.style.backgroundColor = "";
   });
-  missionAbort.addEventListener("click", function (event) {
+  missionAbort.addEventListener("click", function () {
     let response = window.confirm(
       "Are you sure you want to abort the mission?"
     );
