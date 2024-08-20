ab472e972b47913c247b68d0d1773b9da71dc536
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-08-20 11:27:10 -0500

    finished chapter exercises for dom and events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,15 +1,25 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    button.addEventListener('click', function (event) {
-        paragraph.innerHTML = 'Houston! We have liftoff!';
-     });
-     missionAbort.addEventListener("mouseover", function (event) {
-        button.style.backgroundColor = "red" //am I also supposed to be coding in the style.css?  Otherwise, I don't understand how to set this color.
-     });
+  // Put your code for the exercises here.
+  button.addEventListener("click", function (event) {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+  missionAbort.addEventListener("mouseover", function (event) {
+    button.target.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("mouseout", function (event) {
+    button.target.style.backgroundColor = "";
+  });
+  missionAbort.addEventListener("click", function (event) {
+    let response = window.confirm(
+      "Are you sure you want to abort the mission?"
+    );
+    if (response === true) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+    }
+  });
 }
- //when the button is clicked, the <p id="statusReport">The shuttle is on the ground</p> changes to <p id="statusReport">Houston, we have liftoff!</p>
- //when the curson goes over id="abortMission" the button background turns red
+window.addEventListener("load", init);
